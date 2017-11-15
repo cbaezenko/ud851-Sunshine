41b7840b5207451efad752eabd2feba041700994
baeza <cbaezenko@gmail.com>
2017-11-15 15:37:47 +0300

    Add result S04.01

---
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by baeza on 15.11.2017.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
+}
